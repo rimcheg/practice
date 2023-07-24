3c99473c78bff3fc0836b34dd23bb728efa8bdbf
Rimcheg <ZhydkovRuslan@gmail.com>
2023-07-24 22:27:37 +0300

    new file:   ainovaCosmetic/Become_a_club_member.html 	new file:   ainovaCosmetic/css/Become_a_club_member.css 	modified:   ainovaCosmetic/css/home.css 	modified:   ainovaCosmetic/home.html 	new file:   ainovaCosmetic/image/arrow_bot.png 	new file:   ainovaCosmetic/image/arrow_top.png 	new file:   ainovaCosmetic/image/becomeClub.png 	new file:   ainovaCosmetic/image/discount-circle.png 	new file:   ainovaCosmetic/image/icons (1).png 	new file:   ainovaCosmetic/image/icons.png 	new file:   ainovaCosmetic/image/ticket.png 	new file:   ainovaCosmetic/js/Become_a_club_member.js

---
+++ ainovaCosmetic/js/Become_a_club_member.js
@@ -0,0 +1,20 @@
+document.querySelector('.reed_more_1').onclick = function () {
+    document.querySelector('.reed_more_1_img').classList.toggle('rotate_img');
+    document.querySelector('.reed_more_1_text').classList.toggle('vizible_text')
+}
+document.querySelector('.reed_more_2').onclick = function () {
+    document.querySelector('.reed_more_2_img').classList.toggle('rotate_img');
+    document.querySelector('.reed_more_2_text').classList.toggle('vizible_text')
+}
+document.querySelector('.reed_more_3').onclick = function () {
+    document.querySelector('.reed_more_3_img').classList.toggle('rotate_img');
+    document.querySelector('.reed_more_3_text').classList.toggle('vizible_text')
+}
+document.querySelector('.reed_more_4').onclick = function () {
+    document.querySelector('.reed_more_4_img').classList.toggle('rotate_img');
+    document.querySelector('.reed_more_4_text').classList.toggle('vizible_text')
+}
+document.querySelector('.reed_more_5').onclick = function () {
+    document.querySelector('.reed_more_5_img').classList.toggle('rotate_img');
+    document.querySelector('.reed_more_5_text').classList.toggle('vizible_text')
+}
