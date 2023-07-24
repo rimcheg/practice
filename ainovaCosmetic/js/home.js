6fffde0caed22fef2baeef426f93528c6f388f26
Rimcheg <ZhydkovRuslan@gmail.com>
2023-07-24 19:48:14 +0300

    modified:   ainovaCosmetic/css/home.css 	modified:   ainovaCosmetic/home.html 	modified:   ainovaCosmetic/js/home.js

--- ainovaCosmetic/js/home.js
+++ ainovaCosmetic/js/home.js
@@ -1,3 +1,14 @@
 document.querySelector('.popup_menu').onclick = function () {
     document.querySelector('.popup_header').classList.toggle('active_popup_menu');
+    document.querySelector('.content').classList.toggle('blure_all');
+    document.querySelector('.header_content').classList.toggle('blure_all');
+    document.querySelector('.bg_img').classList.toggle('blure_all');
+    document.querySelector('.footer').classList.toggle('blure_all');
+}
+
+document.querySelector('.to_right').onclick = function () {
+    document.querySelector('.absolute').classList.add('right');
+}
+document.querySelector('.to_left').onclick = function () {
+    document.querySelector('.absolute').classList.remove('right');
 }
