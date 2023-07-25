de934ff0caa7929c97abc8e024ceb28823f99ed5
Rimcheg <ZhydkovRuslan@gmail.com>
2023-07-25 13:17:36 +0300

    modified:   ainovaCosmetic/Become_a_club_member.html 	new file:   ainovaCosmetic/become_a_partner.html 	modified:   ainovaCosmetic/css/Become_a_club_member.css 	new file:   ainovaCosmetic/css/become_a_partner.css 	modified:   ainovaCosmetic/css/home.css 	modified:   ainovaCosmetic/home.html 	new file:   ainovaCosmetic/image/Become_a_partner_cont1.png 	new file:   ainovaCosmetic/image/Become_a_partner_cont2.png 	new file:   ainovaCosmetic/image/Become_a_partner_cont3.png 	new file:   ainovaCosmetic/image/Become_a_partner_cont4.png 	new file:   ainovaCosmetic/image/Become_a_partner_cont_video.png 	new file:   ainovaCosmetic/image/Become_a_partner_head.png 	new file:   ainovaCosmetic/image/partner_cart1.png 	new file:   ainovaCosmetic/image/partner_cart2.png 	new file:   ainovaCosmetic/image/partner_cart3.png 	new file:   ainovaCosmetic/js/become_a_partner.js 	modified:   ainovaCosmetic/js/home.js

--- ainovaCosmetic/js/home.js
+++ ainovaCosmetic/js/home.js
@@ -4,6 +4,7 @@ document.querySelector('.popup_menu').onclick = function () {
     document.querySelector('.header_content').classList.toggle('blure_all');
     document.querySelector('.bg_img').classList.toggle('blure_all');
     document.querySelector('.footer').classList.toggle('blure_all');
+    document.querySelector('.popup_menu').classList.toggle('active_link')
 }
 
 document.querySelector('.to_right').onclick = function () {
