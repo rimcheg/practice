2bc024e7b52e3f89240364584f507bbd947cba89
Rimcheg <ZhydkovRuslan@gmail.com>
2023-08-02 22:40:10 +0300

    new file:   climatica/climatica.css 	new file:   climatica/climatica.html 	new file:   climatica/climatica.js 	new file:   climatica/images/Footter_img/facebook.png 	new file:   climatica/images/Footter_img/instagram.png 	new file:   climatica/images/Footter_img/odnoklassniki 1.png 	new file:   climatica/images/Footter_img/skype.png 	new file:   climatica/images/Footter_img/vk.png 	new file:   climatica/images/Group 115.png 	new file:   climatica/images/Header_img/Climatica_logo.png 	new file:   climatica/images/Header_img/List_menu.png 	new file:   climatica/images/Header_img/Search.png 	new file:   climatica/images/Header_img/Telegram.png 	new file:   climatica/images/Header_img/Viber.png 	new file:   climatica/images/Header_img/WhatsApp.png 	new file:   climatica/images/active_link.png

---
+++ climatica/climatica.js
@@ -0,0 +1,124 @@
+// on Site toogles
+
+let home_cl = document.getElementById('Home').classList
+let cat_men_cl = document.getElementById('Open_menu_catalog').classList
+let seas_men_cl = document.getElementById('Open_menu_seazon').classList
+
+document.getElementById('On_click_seazon').onclick = () => {
+    let y = cat_men_cl[1]
+    if (home_cl.length === 1 && y === undefined) {
+        document.querySelector('#Open_menu_seazon').classList.toggle('not_on_display')
+        document.querySelector('#On_click_seazon').classList.toggle('active_link')
+        document.querySelector('.season_img').classList.toggle('display_none')
+        document.querySelector('#Open_menu_catalog').classList.add('not_on_display')
+        document.querySelector('#On_click_catalog').classList.remove('active_link')
+        document.querySelector('.catalog_img').classList.add('display_none')
+    }
+    else if (home_cl.length === 0 && y === 'not_on_display') {
+        document.querySelector('#Open_menu_seazon').classList.toggle('not_on_display')
+        document.querySelector('#On_click_seazon').classList.toggle('active_link')
+        document.querySelector('.season_img').classList.toggle('display_none')
+        document.querySelector('#Open_menu_catalog').classList.add('not_on_display')
+        document.querySelector('#On_click_catalog').classList.remove('active_link')
+        document.querySelector('.catalog_img').classList.add('display_none')
+        document.querySelector('#Home').classList.add('blure')
+        document.querySelector('#Catalog').classList.add('blure')
+        document.querySelector('#card').classList.add('blure')
+        document.querySelector('#News').classList.add('blure')
+        document.querySelector('#News_article').classList.add('blure')
+        document.querySelector('#Payment_delivery').classList.add('blure')
+        document.querySelector('#Contakts').classList.add('blure')
+        document.querySelector('#About_company').classList.add('blure')
+        document.querySelector('#Partners').classList.add('blure')
+        document.querySelector('#Search').classList.add('blure')
+        document.querySelector('#Search_not_found').classList.add('blure')
+        document.querySelector('#FAQ').classList.add('blure')
+        document.querySelector('#Size_table').classList.add('blure')
+        document.querySelector('#Size_table_without_women').classList.add('blure')
+        document.querySelector('#Thank_you_page').classList.add('blure')
+        document.querySelector('#Error_404').classList.add('blure')
+    }
+    else if (home_cl.length === 1 && y === 'not_on_display') {
+        document.querySelector('#Open_menu_seazon').classList.toggle('not_on_display')
+        document.querySelector('#On_click_seazon').classList.toggle('active_link')
+        document.querySelector('.season_img').classList.toggle('display_none')
+        document.querySelector('#Open_menu_catalog').classList.add('not_on_display')
+        document.querySelector('#On_click_catalog').classList.remove('active_link')
+        document.querySelector('.catalog_img').classList.add('display_none')
+        document.querySelector('#Home').classList.remove('blure')
+        document.querySelector('#Catalog').classList.remove('blure')
+        document.querySelector('#card').classList.remove('blure')
+        document.querySelector('#News').classList.remove('blure')
+        document.querySelector('#News_article').classList.remove('blure')
+        document.querySelector('#Payment_delivery').classList.remove('blure')
+        document.querySelector('#Contakts').classList.remove('blure')
+        document.querySelector('#About_company').classList.remove('blure')
+        document.querySelector('#Partners').classList.remove('blure')
+        document.querySelector('#Search').classList.remove('blure')
+        document.querySelector('#Search_not_found').classList.remove('blure')
+        document.querySelector('#FAQ').classList.remove('blure')
+        document.querySelector('#Size_table').classList.remove('blure')
+        document.querySelector('#Size_table_without_women').classList.remove('blure')
+        document.querySelector('#Thank_you_page').classList.remove('blure')
+        document.querySelector('#Error_404').classList.remove('blure')
+    }
+}
+document.getElementById('On_click_catalog').onclick = () => {
+    let y = seas_men_cl[1]
+    if (home_cl.length === 1 && y === undefined) {
+        document.querySelector('#Open_menu_catalog').classList.toggle('not_on_display')
+        document.querySelector('#On_click_catalog').classList.toggle('active_link')
+        document.querySelector('.catalog_img').classList.toggle('display_none')
+        document.querySelector('#Open_menu_seazon').classList.add('not_on_display')
+        document.querySelector('#On_click_seazon').classList.remove('active_link')
+        document.querySelector('.season_img').classList.add('display_none')
+    }
+    else if (home_cl.length === 0 && y === 'not_on_display') {
+        document.querySelector('#Open_menu_catalog').classList.toggle('not_on_display')
+        document.querySelector('#On_click_catalog').classList.toggle('active_link')
+        document.querySelector('.catalog_img').classList.toggle('display_none')
+        document.querySelector('#Open_menu_seazon').classList.add('not_on_display')
+        document.querySelector('#On_click_seazon').classList.remove('active_link')
+        document.querySelector('.season_img').classList.add('display_none')
+        document.querySelector('#Home').classList.add('blure')
+        document.querySelector('#Catalog').classList.add('blure')
+        document.querySelector('#card').classList.add('blure')
+        document.querySelector('#News').classList.add('blure')
+        document.querySelector('#News_article').classList.add('blure')
+        document.querySelector('#Payment_delivery').classList.add('blure')
+        document.querySelector('#Contakts').classList.add('blure')
+        document.querySelector('#About_company').classList.add('blure')
+        document.querySelector('#Partners').classList.add('blure')
+        document.querySelector('#Search').classList.add('blure')
+        document.querySelector('#Search_not_found').classList.add('blure')
+        document.querySelector('#FAQ').classList.add('blure')
+        document.querySelector('#Size_table').classList.add('blure')
+        document.querySelector('#Size_table_without_women').classList.add('blure')
+        document.querySelector('#Thank_you_page').classList.add('blure')
+        document.querySelector('#Error_404').classList.add('blure')
+    }
+    else if (home_cl.length === 1 && y === 'not_on_display') {
+        document.querySelector('#Open_menu_catalog').classList.toggle('not_on_display')
+        document.querySelector('#On_click_catalog').classList.toggle('active_link')
+        document.querySelector('.catalog_img').classList.toggle('display_none')
+        document.querySelector('#Open_menu_seazon').classList.add('not_on_display')
+        document.querySelector('#On_click_seazon').classList.remove('active_link')
+        document.querySelector('.season_img').classList.add('display_none')
+        document.querySelector('#Home').classList.remove('blure')
+        document.querySelector('#Catalog').classList.remove('blure')
+        document.querySelector('#card').classList.remove('blure')
+        document.querySelector('#News').classList.remove('blure')
+        document.querySelector('#News_article').classList.remove('blure')
+        document.querySelector('#Payment_delivery').classList.remove('blure')
+        document.querySelector('#Contakts').classList.remove('blure')
+        document.querySelector('#About_company').classList.remove('blure')
+        document.querySelector('#Partners').classList.remove('blure')
+        document.querySelector('#Search').classList.remove('blure')
+        document.querySelector('#Search_not_found').classList.remove('blure')
+        document.querySelector('#FAQ').classList.remove('blure')
+        document.querySelector('#Size_table').classList.remove('blure')
+        document.querySelector('#Size_table_without_women').classList.remove('blure')
+        document.querySelector('#Thank_you_page').classList.remove('blure')
+        document.querySelector('#Error_404').classList.remove('blure')
+    }
+}
